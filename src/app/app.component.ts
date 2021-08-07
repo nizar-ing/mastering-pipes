1769a362bb060aa90bda4069ee2d5ebab262f7e9
nizar-ing <nizar.ilahi@gmail.com>
2021-08-07 18:46:36 +0100

    adding files for deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-pipes';
+  receiver_name: string;
+  date: string;
+  amount: number;
+  miles: number;
+
+  onInputChange(value: string) {
+    this.receiver_name = value;
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
 }
